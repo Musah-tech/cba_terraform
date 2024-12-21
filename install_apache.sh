c872531d8e2f49ea77b055aa32cd69bd67140584
musah ibrahim <ibramusah@yahoo.co.uk>
2024-12-21 18:00:19 +0000

    my first commit

---
+++ install_apache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo service httpd start
+sudo service httpd status
+sudo echo "Hello World from $(hostname -f)" | sudo tee /var/www/html/index.html
+
+user_data = file("install_apache.sh")
